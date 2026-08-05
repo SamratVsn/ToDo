4dcb680464e6f7da58810751b2126a878d752023
samratvsn <samratvsn@gmail.com>
2026-08-05 09:05:03 +0545

    ViewModel Provider

--- app/src/main/java/com/example/todovsn/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/todovsn/ui/AppViewModelProvider.kt
@@ -1,20 +1,42 @@
 package com.example.todovsn.ui
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.todovsn.ToDoApplication
 import com.example.todovsn.ui.home.HomeViewModel
 import com.example.todovsn.ui.screens.AddViewModel
+import com.example.todovsn.ui.screens.DetailsViewModel
+import com.example.todovsn.ui.screens.EditViewModel
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
 
         initializer {
-            AddViewModel(ToDoApplication().container.toDoRepository)
+            EditViewModel(
+                this.createSavedStateHandle(),
+                toDoApplication().container.toDoRepository
+            )
         }
 
         initializer {
-            HomeViewModel(ToDoApplication().container.toDoRepository)
+            DetailsViewModel(
+                this.createSavedStateHandle(),
+                toDoApplication().container.toDoRepository
+            )
+        }
+
+        initializer {
+            AddViewModel(toDoApplication().container.toDoRepository)
+        }
+
+        initializer {
+            HomeViewModel(toDoApplication().container.toDoRepository)
         }
     }
 }
+
+fun CreationExtras.toDoApplication() : ToDoApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ToDoApplication)
