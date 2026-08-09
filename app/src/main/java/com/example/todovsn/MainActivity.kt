0296abe3a43bf089c6b61a3512469323d7850bbc
samratvsn <samratvsn@gmail.com>
2026-08-09 08:20:28 +0545

    Splash Screen Added To-Do Vsn 1.3.0

--- app/src/main/java/com/example/todovsn/MainActivity.kt
+++ app/src/main/java/com/example/todovsn/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.todovsn.ToDoApp
 import com.example.todovsn.ui.theme.ToDoVsnTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
